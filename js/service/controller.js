0cafbfdaae210fd43fb6edc4765fbec1a42e61e4
Yonatan Hershko <yonher8@gmail.com>
2024-05-30 16:01:31 +0300

    Add render fun

--- js/service/controller.js
+++ js/service/controller.js
@@ -1,13 +1,35 @@
 'use strict'
 
-
+var gNextId = 100
+var gBooks = [
+    { id: gNextId++, title: 'The adventures of Lori Ipsi', price: 120, imgUrl: 'lori-ipsi.jpg' },
+    { id: gNextId++, title: 'The adventures ', price: 150, imgUrl: 'lori-ipsi.jpg' },
+    { id: gNextId++, title: 'The', price: 180, imgUrl: 'lori-ipsi.jpg' }
+]
 
 
 
 function onInit() {
-    
+    render()
 }
 
-function render()  {
-    
+function render() {
+    const elBookList = document.querySelector('tbody')
+
+    const strHtmls = gBooks.map(book => `
+    <tr>
+            <td>${book.title}</td>
+            <td>${book.price}</td>
+            <td><button >Read</button>
+             <button >Update</button> 
+             <button onclick ="onRemoveBook('${book.id}')">Delete</button></td>
+          </tr>`
+    )
+
+    elBookList.innerHTML = strHtmls.join('')
 }
+
+
+
+
+
